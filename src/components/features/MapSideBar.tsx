367a73020b471bed908fe6738991dda9b8a17b44
Vasilis Sakkas <billsakkas@me.com>
2022-10-01 19:12:04 +0300

    Modify components/features/MapSideBar.tsx

--- src/components/features/MapSideBar.tsx
+++ src/components/features/MapSideBar.tsx
@@ -6,7 +6,7 @@ type TMapSideBar = {
 
 const MapSideBar = ({ children }: TMapSideBar) => {
   return (
-    <div className="flex w-60 flex-col gap-3 p-4">
+    <div className="flex w-fit flex-col gap-3">
       <h1 className="text-center text-2xl font-bold text-black">
         Available estates
       </h1>
