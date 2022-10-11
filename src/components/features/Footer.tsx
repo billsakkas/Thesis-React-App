55095a1c12ea5a55e0fd5e2592562e6b0073c73d
Vasilis Sakkas <billsakkas@me.com>
2022-10-11 10:02:06 +0300

    Modify Footer.tsx Header.tsx
    Design update

--- src/components/features/Footer.tsx
+++ src/components/features/Footer.tsx
@@ -4,19 +4,22 @@ type TFooter = {};
 
 const Footer = ({}: TFooter) => {
   return (
-    <div className="flex bg-orange-100 shadow-lg">
-      <div className="flex flex-col gap-2 p-4">
-        <h3 className="text-lg font-bold">
-          SweetDeal -{" "}
-          <span className="text-lg font-light tracking-widest">Github</span>
-        </h3>
-        <p className="text-md font-thin">
-          This project was created just for fun. It is not a real estate
-          application. It is just a project to practice my skills and try new
-          things. However, I believe that it is a good start for a real estate
-          application. I hope you like it.
-        </p>
-      </div>
+    <div className="flex flex-col bg-orange-100 p-4">
+      <h3 className="text-lg font-bold">
+        SweetDeal -{" "}
+        <a
+          className="text-lg font-light tracking-widest"
+          href="https://github.com/billsakkas"
+        >
+          Github
+        </a>
+      </h3>
+      <p className="text-md font-thin">
+        This project was created just for fun. It is not a real estate
+        application. It is just a project to practice my skills and try new
+        things. However, I believe that it is a good start for a real estate
+        application. I hope you like it.
+      </p>
     </div>
   );
 };
--- src/components/features/Header.tsx
+++ src/components/features/Header.tsx
@@ -8,13 +8,13 @@ type THeader = {
 
 const Header = ({ type, searchHeaderInputs }: THeader) => {
   return (
-    <nav className="flex flex-col gap-8 bg-orange-100">
-      <div className="flex items-center justify-between px-2 pt-2">
+    <nav className="flex flex-col gap-8 bg-orange-100 py-2">
+      <div className="flex items-center justify-between px-2">
         <div className="flex items-center gap-2">
-          <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+          <span className="text-2xl">🏘️</span>
           <h1 className="text-lg font-bold">SweetDeal</h1>
         </div>
-        <div className="h-6 w-6 rounded-full bg-gray-300"></div>
+        <span className="text-2xl">🥪</span>
       </div>
       {type && type === "search" && (
         <SearchHeader Inputs={searchHeaderInputs} />
